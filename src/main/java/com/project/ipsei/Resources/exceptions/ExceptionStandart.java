892cee203bdbf6cab391cd4dcd9ad322666548b7
Lucas <silvalucas.me.contact@gmail.com>
2026-03-21 23:53:45 -0300

    feat(Resource - Service): Adiciona algumas Exceptions e ExceptionHandler

---
+++ src/main/java/com/project/ipsei/Resources/exceptions/ExceptionStandart.java
@@ -0,0 +1,4 @@
+package com.project.ipsei.Resources.exceptions;
+
+public class ExceptionStandart {
+}
---
+++ src/main/java/com/project/ipsei/Services/exceptions/ConflicException.java
@@ -0,0 +1,4 @@
+package com.project.ipsei.Services.exceptions;
+
+public class ConflicException {
+}
